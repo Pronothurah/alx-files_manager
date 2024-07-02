0b91fd981a3e176fd84b38fda4e40480e7381a23
Pronothurah <onsongojosphat28@gmail.com>
2024-07-02 17:17:09 +0300

    set up redis.js

---
+++ utils/redis.js
@@ -0,0 +1,52 @@
+const redis = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.client.on('error', (err) => {
+      console.error(`Redis client not connected to the server: ${err}`);
+    });
+
+    this.client.on('connect', () => {
+      console.log('Redis client connected to the server');
+    });
+
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    try {
+      return await this.getAsync(key);
+    } catch (err) {
+      console.error(`Error getting value from Redis: ${err}`);
+      return null;
+    }
+  }
+
+  async set(key, value, duration) {
+    try {
+      await this.setAsync(key, value, 'EX', duration);
+    } catch (err) {
+      console.error(`Error setting value in Redis: ${err}`);
+    }
+  }
+
+  async del(key) {
+    try {
+      await this.delAsync(key);
+    } catch (err) {
+      console.error(`Error deleting value from Redis: ${err}`);
+    }
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
